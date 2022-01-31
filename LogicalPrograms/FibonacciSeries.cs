9d3656d9721e4898dede3083501478b9d46c40d7
vismitap <vismita.prabhu@gmail.com>
2022-02-01 00:39:51 +0530

    [Vismita] Add: UC1-FibonacciSeries solved

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public void GenerateFibonacciSeries()
+        {
+            Console.WriteLine("Enter the number of elements in fibonacci series you want: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int[] fiboArr = new int[num];
+            fiboArr[0] = 0;
+            fiboArr[1] = 1;
+
+            for(int i = 2; i < num; i++)
+            {
+                fiboArr[i] = fiboArr[i-1] + fiboArr[i-2];
+            }
+
+            for(int i = 0; i < num; i++)
+            {
+                Console.Write(fiboArr[i]+" ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -10,7 +10,34 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine("Welcome to Logical Programs\n");
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("****************");
+                Console.WriteLine("Enter\n0 for Exit\n" +
+               "1 for Fibonacci Series \n");
+
+                Console.WriteLine("****************\n");
+
+                Console.WriteLine("Enter your option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (option)
+                {
+                    case 0:
+                        flag = false;
+                        break;
+
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.GenerateFibonacciSeries();
+                        break;
+
+                }
+            }
         }
     }
 }
